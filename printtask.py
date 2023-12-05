05b9bfd2f44873cb7f02feaafdb835cbf70ebe22
Brian Nganga <briannganga70@gmail.com>
2023-12-05 21:56:31 +0300

    intial

---
+++ printtask.py
@@ -0,0 +1,11 @@
+import psutil
+
+def end_task(process_name):
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'] == process_name:
+            pid = process.info['pid']
+            psutil.Process(pid).terminate()
+            print(f"Successfully ended {process_name} (PID: {pid})")
+
+# Example: end the task for Notepad
+end_task("notepad.exe")
